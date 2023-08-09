47ee77053c596065f7857de29a116bb71d775311
juliamelian <mariajuliamelian@gmail.com>
2023-08-09 00:13:54 -0300

    solución-click-3
    eliminé el alert del html y lo agregué en el js con un nuevo addEventListener al que le agregué un stopPropagation para que al apretar el botón solo se active el primer alert y no se ejecute también el del div.

--- eventos.js
+++ eventos.js
@@ -1,11 +1,19 @@
 var div = document.querySelector("div");
+var button = document.querySelector("button");
 
 div.addEventListener(
   "click",
   function(){
-    stopPropagation();
-    alert('Hola! Soy el div');
+  alert('Hola! Soy el div');
 });
+
+button.addEventListener(
+  "click",
+  function(event){
+    event.stopPropagation();
+    alert('Hola!');
+});
+
 
 
 
